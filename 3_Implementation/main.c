5f519661ee7fac402ff97ab229f33566da8bb210
Mukesh Prajapati <49980427+Mukesh9793@users.noreply.github.com>
2021-11-25 12:32:43 +0530

    Update main.c

--- 3_Implementation/main.c
+++ 3_Implementation/main.c
@@ -1,163 +1,124 @@
 #include <stdio.h>  
 #include <math.h>  
-#include <stdlib.h>  
-  
+#include <stdlib.h>   
 int main()  
-{
-  
-    int ope, num1, num2;
-  
-    float res;
-  
-    char ch;
-  
-    do
-      {
-  
-        printf (" Select an operation to perform: ");
-  
-        printf (" \n 1 Addition  \t \t 2 Subtraction \n 3 Multiplication \t 4 Division \n 5 Square \t \t 6 Square Root \n 7 Exit \n \n Select an operation ");
-
-      
-    scanf ("%d", &ope);
-
-      switch (ope)
-  
-    {
-  
-        case 1:  
-            // Add two numbers
-  
-            printf (" You chose: Addition");
-  
-            printf ("\n Enter First Number: ");
-  
-            scanf (" %d", &num1);
-  
-            printf (" Enter Second Number: ");
-  
+{  
+    int ope, num1, num2;  
+    float res;  
+    char ch;  
+    do  
+    {  
+        printf (" Select an operation to perform the calculation: ");  
+        printf (" \n 1 Addition  \t \t 2 Subtraction \n 3 Multiplication \t 4 Division \n 5 Square \t \t 6 Square Root \n  7 Percent \n 8 Exit \n \n Please, Make a choice ");      
+        scanf ("%d", &ope);     
+    switch (ope)  
+    {  
+        case 1:           // Add two numbers  
+            printf (" You chose: Addition");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);  
+            printf (" Enter Second Number: ");  
+            scanf (" %d", &num2);  
+            int sum(int n1, int n2) {
+                    int num3;
+                    num3 = n1 + n2;
+                     return (num3);
+                    }
+            res = sum(num1,num2);
+            printf (" Addition of two numbers is: %.2f", res);  
+            break;                
+        case 2:            // Subtract two numbers  
+            printf (" You chose: Subtraction");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);  
+            printf (" Enter Second Number: ");  
+            scanf (" %d", &num2); 
+            int sub(int n1, int n2) {
+                    int num3;
+                    num3 = n1 - n2;
+                     return (num3);
+                    }
+            res = sub(num1,num2);  
+            printf (" Subtraction of two numbers is: %.2f", res);  
+            break;             
+        case 3:      // Multiplication of the numbers  
+            printf (" You chose: Multiplication");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);  
+            printf (" Enter Second Number: ");  
             scanf (" %d", &num2);
-  
-            res = num1 + num2;
- 
-             printf (" Addition of two numbers is: %.2f", res);
-  
-            break;
-          case 2: 
-            // Subtract two numbers
-  
-            printf (" You chose: Subtraction");
-  
-            printf ("\n Enter First Number: ");
-  
+            int mul(int n1, int n2) {
+                    int num3;
+                    num3 = n1 * n2;
+                     return (num3);
+                    }
+            res = mul(num1,num2); 
+            printf (" Multiplication of two numbers is: %.2f", res);  
+            break;                
+        case 4:            // Division of the numbers  
+            printf (" You chose: Division");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);  
+            printf (" Enter Second Number: ");  
+            scanf (" %d", &num2);  
+            if (num2 == 0)  
+                {  
+                    printf (" \n Divisor cannot be zero. Please enter another value ");  
+                    scanf ("%d", &num2);        
+                }  
+            int div(int n1, int n2) {
+                    int num3;
+                    num3 = n1 / n2;
+                     return (num3);
+                    }
+            res = div(num1,num2); 
+            printf (" Division of two numbers is: %.2f", res);  
+            break;               
+        case 5:            // Square of a number  
+            printf (" You chose: Square");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);  
+            int sq(int n1) {
+                    int num3;
+                    num3 = n1* n1;
+                     return (num3);
+                    }
+            res = sq(num1); 
+            printf (" Square of %d number is: %.2f", num1, res);  
+            break;                
+        case 6:          // Square root of the number  
+            printf (" You chose: Square Root");  
+            printf ("\n Enter First Number: ");  
+            scanf (" %d", &num1);        
+            res = sqrt(num1);    
+            printf (" Square Root of %d numbers is: %.2f", num1, res);  
+            break;         
+         case 7:
+            printf("You chose: Percentage");
+            printf("Enter First Number: ");
             scanf (" %d", &num1);
-  
-            printf (" Enter Second Number: ");
-  
-            scanf (" %d", &num2);
-  
-            res = num1 - num2;
-  
-            printf (" Subtraction of two numbers is: %.2f", res);
-  
-            break;
-          case 3:  
-            // Multiplication of the numbers
-  
-            printf (" You chose: Multiplication");
-  
-            printf ("\n Enter First Number: ");
-  
-            scanf (" %d", &num1);
-  
-            printf (" Enter Second Number: ");
-  
-            scanf (" %d", &num2);
-  
-            res = num1 * num2;
- 
-             printf (" Multiplication of two numbers is: %.2f", res);
-  
-            break;
-          case 4:   
-            // Division of the numbers
-  
-            printf (" You chose: Division");
-  
-            printf ("\n Enter First Number: ");
-  
-            scanf (" %d", &num1);
-  
-            printf (" Enter Second Number: ");
-  
-            scanf (" %d", &num2);
-  
-            if (num2 == 0)
-  
-                {
-  
-                    printf (" \n Divisor cannot be zero. Please enter another value ");
-  
-                    scanf ("%d", &num2);
-        
-                }
-  
-            res = num1 / num2;
- 
-             printf (" Division of two numbers is: %.2f", res);
-  
-            break;
-            case 5:  
-            // Square of a number
-  
-            printf (" You chose: Square");
-  
-            printf ("\n Enter First Number: ");
-  
-            scanf (" %d", &num1);
-  
-  
-          res = num1 * num1;
- 
-             printf (" Square of %d number is: %.2f", num1, res);
-  
-            break;
-            case 6:  
-            // Square root of the number
-  
-            printf (" You chose: Square Root");
-  
-            printf ("\n Enter First Number: ");
-  
-            scanf (" %d", &num1);
-  
-             res = sqrt(num1);
-    
-          printf (" Square Root of %d numbers is: %.2f", num1, res);
-  
-            break;
-            case 7:
-  
-            printf (" You chose: Exit");
-  
-            exit(0);
-   
-            break;
- 
-             default:
-  
-            printf(" Something is wrong!! ");
-  
-            break;
-          }  
-    
-            printf (" \n \n --------------------------------------------- \n ");
-  
-    }
- 
-    while (ope != 7);
-  
-  
-    return 0;
-        
+            printf("Enter Second Number: ");
+            scanf (" %d", &num2);          
+            int per(int n1, int n2) {
+                    float num3;
+                    float temp;
+                    temp=(float)n2/100.0;
+                    num3=temp*n1;
+                     return (num3);
+                    }
+            res=per(num1,num2);
+            printf("%d percent of %d is : %.2f", num2,num1,res);
+            break;          
+        case 8:  
+            printf (" You chose: Exit");  
+            exit(0);   
+            break;             
+        default:  
+            printf(" Something is wrong!! ");  
+            break;                        
+    }  
+    printf (" \n \n --------------------------------------------- \n ");  
+    } 
+    while (ope != 8);   
+    return 0;        
 }
